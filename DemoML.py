60dc74e4cca22bfd0d85aabe6e27eeea16727f6c
Tejas Pillai <tejaspillai@Tejas.local>
2017-12-23 11:27:30 +0100

    updated Readme

--- DemoML.py
+++ DemoML.py
@@ -1,14 +1,12 @@
 from sklearn import tree
 
-clf = tree.DecisionTreeClassifier()
+classifier = tree.DecisionTreeClassifier()
 
 # CHALLENGE - create 3 more classifiers...
-# 1
-# 2
-# 3
+
 
 # [height, weight, shoe_size]
-X = [[181, 80, 44], [177, 70, 43], [160, 60, 38], [154, 54, 37], [166, 65, 40],
+X = [[182, 80, 44], [176, 70, 43], [160, 60, 38], [154, 54, 37], [166, 65, 40],
      [190, 90, 47], [175, 64, 39],
      [177, 70, 40], [159, 55, 37], [171, 75, 42], [181, 85, 43]]
 
@@ -16,11 +14,11 @@ Y = ['male', 'male', 'female', 'female', 'male', 'male', 'female', 'female',
      'female', 'male', 'male']
 
 
-# CHALLENGE - ...and train them on our data
-clf = clf.fit(X, Y)
+#  train them on our data
+updatedClassifier = classifier.fit(X, Y)
 
-prediction = clf.predict([[190, 70, 43]])
+myprediction = updatedClassifier.predict([[192, 70, 43]])
 
-# CHALLENGE compare their reusults and print the best one!
+# compare their results and print the best one!
 
-print(prediction)
+print(myprediction)
--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # ClassificationML
+python DemoML.py 
